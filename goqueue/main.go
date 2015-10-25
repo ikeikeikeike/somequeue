f7a7a4468c3276a66941c56878d6313bc0ce3963
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-10-26 03:54:59 +0900

    rename

--- goqueue/main.go
+++ goqueue/main.go
@@ -2,27 +2,24 @@ package main
 
 import (
 	"os"
-
 	"github.com/jrallison/go-workers"
 )
 
-type Map map[string]interface{}
-type Maps []Map
+type M map[string]interface{}
 
 func main() {
 	workers.Configure(map[string]string{
 		"server":   "localhost:6379",
 		"database": "0",
 		"process":  "0",
-		"pool":     "30",
 	})
 
-	workers.Enqueue("default", "ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper", Maps{
-		Map{
+	workers.Enqueue("default", "ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper", []M{
+		M{
 			"queue_name": "default",
 			"job_class":  "EchoJob",
-			"arguments": Maps{
-				Map{"goqueue": os.Args[1]},
+			"arguments": []M{
+				M{"goqueue": os.Args[1]},
 			},
 		},
 	})
