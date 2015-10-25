9c110344b0639487cbfa3bb347d31528b8272462
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-10-26 02:00:53 +0900

    rename

--- exenqueue.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in `seq 0 10000`; do
-    sleep 5 
-
-    echo "mix run -e \"Exq.enqueue(:exq, 'default', 'ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper', [%{'arguments' => [%{'exqueue' => $i}], 'queue_name' => 'default', 'job_class' => 'EchoJob'}])\""
-
-    (cd exqueue; bash mix run -e "Exq.enqueue(:exq, \"default\", \"ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper\", [%{\"arguments\" => [%{\"exqueue\" => $i}], \"queue_name\" => \"default\", \"job_class\" => \"EchoJob\"}])" )
-done
