2a039ecfa0fdbf191d15585e78e3ff5146ebda47
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-10-26 09:47:51 +0900

    install or update script

---
+++ prepare.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+(cd ./rbqueue
+
+# NOKOGIRI_USE_SYSTEM_LIBRARIES=1 \
+    rbenv exec bundle install --path vendor/bundle
+)
+
+
+(cd ./exqueue
+
+mix deps.get 
+
+)
+
+
+(cd ./goqueue
+
+go get -v -d
+
+)
