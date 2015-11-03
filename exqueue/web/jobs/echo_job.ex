ae15ea31de724533a9f54f6697173a0400bff616
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-11-04 02:27:23 +0900

    remove

--- exqueue/web/jobs/echo_job.ex
+++
@@ -1,9 +0,0 @@
-defmodule ActiveJob.QueueAdapters.SidekiqAdapter.JobWrapper do
-  def perform(%{"job_class"=>"EchoJob", "queue_name"=>"default", "arguments"=>arguments}) do
-    IO.inspect(arguments)
-  end
-
-  def perform(msg) when is_map(msg) do
-    IO.inspect(msg)
-  end
-end
