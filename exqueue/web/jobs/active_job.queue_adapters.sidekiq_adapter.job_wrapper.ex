bbfded42b5a390c01c707cb1798cfa29d8d57a73
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-11-04 09:34:46 +0900

    fixed job

--- exqueue/web/jobs/active_job.queue_adapters.sidekiq_adapter.job_wrapper.ex
+++ exqueue/web/jobs/active_job.queue_adapters.sidekiq_adapter.job_wrapper.ex
@@ -1,11 +1,6 @@
 defmodule ActiveJob.QueueAdapters.SidekiqAdapter.JobWrapper do
   use Exqueue.Web, :worker
 
-  def perform(%{"job_class"=>"EchoJob", "queue_name"=>"default", "arguments"=>arguments}) do
-    IO.inspect(arguments)
-  end
-
-  def perform(msg) when is_map(msg) do
-    IO.inspect(msg)
-  end
+  def perform(%{"job_class"=>"EchoJob", "queue_name"=>"default", "arguments"=>args}), do: args |> IO.inspect
+  def perform(args) when is_map(args), do: args |> IO.inspect
 end
